bd5998e36615515709fbdfdd1220a12ecf3a710c
Alex Thalhammer <alex@angular-software.at>
2021-10-13 09:23:13 +0200

    [Routing] Add Routing

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { SharedModule } from '../shared/shared.module';
+
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
 import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
@@ -12,9 +15,12 @@ import { MultiFieldValidatorDirective } from './shared/validation/multi-field-va
 import { AsyncMultiFieldValidatorDirective } from './shared/validation/async-multi-field-validator.directive';
 
 import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+
+import { FLIGHT_BOOKING_ROUTES } from './flight-booking.routes';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(FLIGHT_BOOKING_ROUTES), SharedModule],
   declarations: [
     FlightSearchComponent,
     FlightCardComponent,
@@ -24,7 +30,8 @@ import { FlightEditComponent } from './flight-edit/flight-edit.component';
     AsyncCityValidatorDirective,
     MultiFieldValidatorDirective,
     AsyncMultiFieldValidatorDirective,
-    FlightEditComponent
+    FlightEditComponent,
+    PassengerSearchComponent
   ],
   providers: [],
   exports: [FlightSearchComponent]
